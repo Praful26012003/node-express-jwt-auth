4dea7c42355d0f5dfccf35b00be24742590bfade
Praful26012003 <praful26.pbps@gmail.com>
2023-09-03 22:24:36 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { default: isEmail } = require("validator/lib/isEmail");
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    email : {
+        type: String,
+        required: [true, "Please enter an email"],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, "Please enter a valid email"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+        minlength: [6, "Password must be atleast of length 6"]
+    }
+})
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+
+//static method
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        return user;
+      }
+      throw error('incorrect password');
+    }
+    throw error('incorrect email');
+  };
+  
+
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
